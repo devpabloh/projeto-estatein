8a20947cc42cfe0c6bbcfe52999d2b6fbfe59364
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-01-18 09:03:59 -0300

    :bricks: ci: Organizendo a estrutura do componente Logo

--- meu-projeto/src/components/Logo/index.jsx
+++ meu-projeto/src/components/Logo/index.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom"
 
+/* CSS */
+import style from "./Logo.module.css"
+
 /* Logo */
 import logo from "../../assets/Symbol.png"
 
 const Logo = () => {
   return (
-    <Link to="/">
+    <Link to="/" className={style.containerLogo}>
         <img src={logo} alt="Logo" />
         <span>Estatein</span>
     </Link>
